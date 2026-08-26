692c01b95e913596344395f466592a78417d64a6
Parth252 <parthlokhande24@gmail.com>
2026-08-26 19:34:49 +0530

    adding invocation for bootstrap node

--- k8s/main.tf
+++ k8s/main.tf
@@ -9,3 +9,20 @@ data "terraform_remote_state" "compute" {
     use_lockfile = true
   }
 }
+
+resource "aws_ssm_association" "k8s_bootstrap" {
+  for_each = data.terraform_remote_state.compute.outputs.node_instance_ids
+
+  name = "AWS-RunShellScript"
+
+  association_name = "kubernetes-bootstrap-${each.key}"
+
+  targets {
+    key    = "InstanceIds"
+    values = [each.value]
+  }
+
+  parameters = {
+    commands = file("${path.module}/scripts/bootstrap-node.sh")
+  }
+}
